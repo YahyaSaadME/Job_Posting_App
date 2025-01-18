e484cc9354c6c9a08fc26c188b163047f328f463
YahyaSaadME <yahyasaad.me@gmail.com>
2025-01-18 12:30:48 +0530

    Image & Resume Upload fixes

---
+++ src/app/api/image/[file]/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from 'next/server';
+import path from 'path';
+
+export async function GET(request: NextRequest) {
+  try {
+    const { pathname } = new URL(request.url);
+    const filename = pathname.split('/').pop();
+    const image = path.join(process.cwd(), './assets/images/'+filename);
+    if(image){
+
+      const fs = require('fs');
+      const fileStream = fs.createReadStream(image);
+      return new NextResponse(fileStream, {
+        headers: {
+          'Content-Type': 'image/jpeg',
+        },
+      });
+    }else{
+      return NextResponse.json(
+        { error: 'Image not found' },
+        { status: 400 }
+      );
+    }
+
+  } catch (error) {
+    console.error('Upload error:', error);
+    return NextResponse.json(
+      { error: 'Error on loading file' },
+      { status: 500 }
+    );
+  }
+}
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
